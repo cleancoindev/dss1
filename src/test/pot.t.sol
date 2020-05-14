15f9984052ee6f911cfab4b1f6d7fe3ebf37a66f
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2020-05-14 13:53:41 -1000

    Update pot.t.sol

--- src/test/pot.t.sol
+++ src/test/pot.t.sol
@@ -42,16 +42,16 @@ contract DSRTest is DSTest {
         vat.hope(address(pot));
     }
     function test_save_0d() public {
-        assertEq(vat.dai(self), rad(100 ether));
+        assertEq(vat.tao(self), rad(100 ether));
 
         pot.join(100 ether);
-        assertEq(wad(vat.dai(self)),   0 ether);
+        assertEq(wad(vat.tao(self)),   0 ether);
         assertEq(pot.pie(self),      100 ether);
 
         pot.drip();
 
         pot.exit(100 ether);
-        assertEq(wad(vat.dai(self)), 100 ether);
+        assertEq(wad(vat.tao(self)), 100 ether);
     }
     function test_save_1d() public {
         pot.join(100 ether);
@@ -60,19 +60,19 @@ contract DSRTest is DSTest {
         pot.drip();
         assertEq(pot.pie(self), 100 ether);
         pot.exit(100 ether);
-        assertEq(wad(vat.dai(self)), 105 ether);
+        assertEq(wad(vat.tao(self)), 105 ether);
     }
     function test_drip_multi() public {
         pot.join(100 ether);
         pot.file("dsr", uint(1000000564701133626865910626));  // 5% / day
         hevm.warp(now + 1 days);
         pot.drip();
-        assertEq(wad(vat.dai(potb)),   105 ether);
+        assertEq(wad(vat.tao(potb)),   105 ether);
         pot.file("dsr", uint(1000001103127689513476993127));  // 10% / day
         hevm.warp(now + 1 days);
         pot.drip();
         assertEq(wad(vat.sin(vow)), 15.5 ether);
-        assertEq(wad(vat.dai(potb)), 115.5 ether);
+        assertEq(wad(vat.tao(potb)), 115.5 ether);
         assertEq(pot.Pie(),          100   ether);
         assertEq(pot.chi() / 10 ** 9, 1.155 ether);
     }
@@ -96,14 +96,14 @@ contract DSRTest is DSTest {
         hevm.warp(now + 1 days);
         pot.drip();
         pot.exit(50 ether);
-        assertEq(wad(vat.dai(self)), 52.5 ether);
+        assertEq(wad(vat.tao(self)), 52.5 ether);
         assertEq(pot.Pie(),          50.0 ether);
 
         pot.file("dsr", uint(1000001103127689513476993127));  // 10% / day
         hevm.warp(now + 1 days);
         pot.drip();
         pot.exit(50 ether);
-        assertEq(wad(vat.dai(self)), 110.25 ether);
+        assertEq(wad(vat.tao(self)), 110.25 ether);
         assertEq(pot.Pie(),            0.00 ether);
     }
     function test_fresh_chi() public {
@@ -116,7 +116,7 @@ contract DSRTest is DSTest {
         assertEq(pot.pie(self), 100 ether);
         pot.exit(100 ether);
         // if we exit in the same transaction we should not earn DSR
-        assertEq(wad(vat.dai(self)), 100 ether);
+        assertEq(wad(vat.tao(self)), 100 ether);
     }
     function testFail_stale_chi() public {
         pot.file("dsr", uint(1000000564701133626865910626));  // 5% / day
